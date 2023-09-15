60997c6f2940a3e742b91e0acc83394c7e4acaf8
Ali <alipalvane@gmail.com>
2023-09-15 11:05:44 +0330

    finish send message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3001");
 
 const App = () => {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
 
   const sendMessage = () => {
     //Send Message to Server
-    socket.emit("send_message", { message: "hello" });
+    socket.emit("send_message", { message });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     //Get Message from Server
-    socket.on("receive_message", (data)=>{
-      alert(data.message)
-
-    })
-  },[socket]) 
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   return (
     <div>
-      <input type="text" placeholder="message" />
+      <input
+        type="text"
+        placeholder="message"
+        onChange={(e) => setMessage(e.target.value)}
+      />
       <button onClick={sendMessage}>Send Message</button>
+      <h2>Messages:</h2>
+      <p>{messageReceived}</p>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
+// Library CORS for handle cors error in my app
 app.use(cors());
 
+//create sever to get and send
 const server = http.createServer(app);
 
+//create instance from socket.io to work with server
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -18,11 +21,14 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  //Get message from client to send
   socket.on("send_message", (data) => {
-    socket.broadcast.emit("receive_message", data)
+    //Send Message to Other Client
+    socket.broadcast.emit("receive_message", data);
   });
 });
 
+// Run simulate Server
 server.listen(3001, () => {
   console.log("SERVER IS RUNNING");
 });
